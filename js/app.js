663a76e2ddeef9fcd90e506fb2c70e5e4f997dfc
ethan375 <ethanebel1@gmail.com>
2018-03-14 16:29:22 -0500

    inital commit project in the final stages

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// console.log("these are connected!");
+const app = angular.module('portfolio', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/skills', {
+    templateUrl: './views/skills.html'
+  }).when('/projects', {
+    templateUrl: './views/projects'
+  }).otherwise({
+    templateUrl: './views/about.html'
+  })
+}]);
